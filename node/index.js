55432fe01f3ce7542713f477d9fd2cce4aa20d42
federzvz <85709865+federzvz@users.noreply.github.com>
2022-09-26 19:50:25 -0300

    Fix: Path is not defined

--- node/index.js
+++ node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Importar las rutas de los productos
 const pastura = require('./routes/pastura.route'); 
