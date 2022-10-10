41c293fa2c1ca37ed3e89c6be99121d27e82b51b
federzvz <85709865+federzvz@users.noreply.github.com>
2022-10-10 20:26:24 -0300

    Delete Endpoint

--- node/controllers/pastura.controller.js
+++ node/controllers/pastura.controller.js
@@ -1,87 +1,97 @@
-var mongoose = require('mongoose');
-const Pastura = require('../models/pastura.model');
+var mongoose = require("mongoose");
+const Pastura = require("../models/pastura.model");
 
 exports.emptyRoute = function (req, res) {
-    res.setHeader('Content-Range', 'pasturas 0-20/20');
-    res.json({});
+  res.setHeader("Content-Range", "pasturas 0-20/20");
+  res.json({});
 };
 
 exports.test = function (req, res) {
-    res.send('API PASTURA BACKEND ANDANDO');
+  res.send("API PASTURA BACKEND ANDANDO");
 };
 
-exports.pastura_create = function (req, res,next) {
-    let pastura = new Pastura(
-        {
-            _id: new mongoose.Types.ObjectId(),
-            Familia: "Gramineas",
-            Especie: req.body.Especie,
-            TipoVegetativo: req.body.TipoVegetativo,
-            RizomaEngrosado: req.body.RizomaEngrosado,
-            Mocollo1: req.body.Mocollo1,
-            Mocollo2: req.body.Mocollo2,
-            ConsistenciaLigula: req.body.ConsistenciaLigula,
-            FormaLigula: req.body.FormaLigula,
-            Tamano: req.body.Tamano,
-            OtrasCaracteristicasLigula: req.body.OtrasCaracteristicasLigula,
-            ColorLigula: req.body.ColorLigula,
-            FormaLamina: req.body.FormaLamina,
-            Canaliculada: req.body.Canaliculada,
-            TipoLamina: req.body.TipoLamina,
-            Apice: req.body.Apice,
-            NervaduraCentralMarcada: req.body.NervaduraCentralMarcada,
-            Observaciones: req.body.Observaciones,
-            Pelos: req.body.Pelos,
-            UbicacionPelos: req.body.UbicacionPelos,
-            Observacion: req.body.Observacion,
-            ObservacionesGenerales: req.body.ObservacionesGenerales,
-            CicloVida: req.body.CicloVida,
-            CicloProductivo: req.body.CicloProductivo,
-            TipoProductivo: req.body.TipoProductivo,
-            TipoCampo: req.body.TipoCampo,
-        }
-    );
+exports.pastura_create = function (req, res, next) {
+  let pastura = new Pastura({
+    _id: new mongoose.Types.ObjectId(),
+    Familia: "Gramineas",
+    Especie: req.body.Especie,
+    TipoVegetativo: req.body.TipoVegetativo,
+    RizomaEngrosado: req.body.RizomaEngrosado,
+    Mocollo1: req.body.Mocollo1,
+    Mocollo2: req.body.Mocollo2,
+    ConsistenciaLigula: req.body.ConsistenciaLigula,
+    FormaLigula: req.body.FormaLigula,
+    Tamano: req.body.Tamano,
+    OtrasCaracteristicasLigula: req.body.OtrasCaracteristicasLigula,
+    ColorLigula: req.body.ColorLigula,
+    FormaLamina: req.body.FormaLamina,
+    Canaliculada: req.body.Canaliculada,
+    TipoLamina: req.body.TipoLamina,
+    Apice: req.body.Apice,
+    NervaduraCentralMarcada: req.body.NervaduraCentralMarcada,
+    Observaciones: req.body.Observaciones,
+    Pelos: req.body.Pelos,
+    UbicacionPelos: req.body.UbicacionPelos,
+    Observacion: req.body.Observacion,
+    ObservacionesGenerales: req.body.ObservacionesGenerales,
+    CicloVida: req.body.CicloVida,
+    CicloProductivo: req.body.CicloProductivo,
+    TipoProductivo: req.body.TipoProductivo,
+    TipoCampo: req.body.TipoCampo,
+  });
 
-    pastura.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.send('Pastura creado satisfactoriamente')
-    })
-}
+  pastura.save(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.send("Pastura creado satisfactoriamente");
+  });
+};
 
-exports.pastura_getAll = function (req, res,next) {
-    Pastura.find({}, function (err, pasturas) {
-        if (err) return next(err);
-        res.json(pasturas);
-    });
-}
+exports.pastura_getAll = function (req, res, next) {
+  Pastura.find({}, function (err, pasturas) {
+    if (err) return next(err);
+    res.json(pasturas);
+  });
+};
 exports.pastura_get = function (req, res) {
-    Pastura.find({}, function (err, pasturas) {
-        if (err) return next(err);
-        res.json(pasturas);
-    });
-}
+  Pastura.find({}, function (err, pasturas) {
+    if (err) return next(err);
+    res.json(pasturas);
+  });
+};
 
 exports.pastura_pasturas = function (req, res) {
-    Pastura.find({}, function (err, pasturas) {
-        if (err) return next(err);
-        res.setHeader('X-Total-Count', pasturas.length);
-        res.setHeader('Content-Range', 'pasturas 0-20/' + pasturas.length);
-        res.send(pasturas);
-    });
-}
+  Pastura.find({}, function (err, pasturas) {
+    if (err) return next(err);
+    res.setHeader("X-Total-Count", pasturas.length);
+    res.setHeader("Content-Range", "pasturas 0-20/" + pasturas.length);
+    res.send(pasturas);
+  });
+};
 
 exports.pastura_edit = function (req, res, next) {
-    Pastura.find({}, function(err, pasturas) {
-        var pasturaMap = [];
-        pasturas.forEach(function(pastura) {
-            if(pastura.id == req.params.id){
-                pasturaMap.push(pastura);
-            }
+  Pastura.find({}, function (err, pasturas) {
+    var pasturaMap = [];
+    pasturas.forEach(function (pastura) {
+      if (pastura.id == req.params.id) {
+        pasturaMap.push(pastura);
+      }
+    });
+    res.send(pasturaMap[0]);
+  });
+};
+
+exports.pastura_delete = function (req, res, next) {
+  Pastura.find({}, function (err, pasturas) {
+    pasturas.forEach(function (pastura) {
+      if (pastura.id == req.params.id) {
+        console.log(pastura._id);
+        Pastura.findByIdAndRemove(pastura._id, function (err) {
+          if (err) return next(err);
+          res.send("Pastura eliminado satisfactoriamente");
         });
-        res.send(
-            pasturaMap[0]
-        );
-    })
+      }
+    });
+  });
 };
--- node/routes/pastura.route.js
+++ node/routes/pastura.route.js
@@ -9,5 +9,6 @@ router.post('/getAll', pastura_controller.pastura_getAll);
 router.get('/all', pastura_controller.pastura_get);
 router.get('/pasturas', pastura_controller.pastura_pasturas);
 router.get('/pasturas/:id', pastura_controller.pastura_edit);
+router.delete('/pasturas/:id', pastura_controller.pastura_delete);
 
 module.exports = router;
