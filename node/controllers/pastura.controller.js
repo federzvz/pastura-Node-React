414f1d708bb261940c7917992e20b0fc15523bdc
Milton <88519142+AyalaTon@users.noreply.github.com>
2022-11-01 21:16:19 -0300

    UpdatePasturaController
    GET Pastura with filters right now

--- node/controllers/pastura.controller.js
+++ node/controllers/pastura.controller.js
@@ -69,8 +69,124 @@ exports.pastura_get = function (req, res) {
 };
 
 exports.pastura_pasturas = function (req, res) {
+  console.log(req.query);
+  
   Pastura.find({}, function (err, pasturas) {
     if (err) return next(err);
+    if(req.query.RizomaEngrosado == 'true'){
+      pasturas = pasturas.filter(pastura => pastura.RizomaEngrosado == true);
+    } else if (req.query.RizomaEngrosado == 'false') {
+      pasturas = pasturas.filter(pastura => pastura.RizomaEngrosado == false);
+    }
+
+    if(req.query.NervaduraCentralMarcada == 'true'){
+      pasturas = pasturas.filter(pastura => pastura.NervaduraCentralMarcada == true);
+    } else if (req.query.NervaduraCentralMarcada == 'false') {
+      pasturas = pasturas.filter(pastura => pastura.NervaduraCentralMarcada == false);
+    }
+
+    if(req.query.TipoVegetativo == 'Cespitoso'){
+      pasturas = pasturas.filter(pastura => pastura.TipoVegetativo == 'Cespitoso');
+    } else if (req.query.TipoVegetativo == 'Estolonifero') {
+      pasturas = pasturas.filter(pastura => pastura.TipoVegetativo == 'Estolonifero');
+    } else if (req.query.TipoVegetativo == 'Rizomatozo') {
+      pasturas = pasturas.filter(pastura => pastura.TipoVegetativo == 'Rizomatozo');
+    } else if (req.query.TipoVegetativo == 'Estolonifero-rizomatozo') {
+      pasturas = pasturas.filter(pastura => pastura.TipoVegetativo == 'Estolonifero-rizomatozo');
+    }
+
+    if(req.query.Mocollo1 == 'Semi rollizo'){
+      pasturas = pasturas.filter(pastura => pastura.Mocollo1 == 'Semi rollizo');
+    } else if (req.query.Mocollo1 == 'Comprimido') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo1 == 'Comprimido');
+    } else if (req.query.Mocollo1 == 'Semi Rolliza') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo1 == 'Semi Rolliza');
+    } else if (req.query.Mocollo1 == 'Rollizo') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo1 == 'Rollizo');
+    }
+
+    if(req.query.Mocollo2 == 'Intravaginal'){
+      pasturas = pasturas.filter(pastura => pastura.Mocollo2 == 'Intravaginal');
+    } else if (req.query.Mocollo2 == 'Extravaginal') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo2 == 'Extravaginal');
+    } else if (req.query.Mocollo2 == 'Extravaginal principalmente') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo2 == 'Extravaginal principalmente');
+    } else if (req.query.Mocollo2 == 'Intra o extra') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo2 == 'Intra o extra');
+    } else if (req.query.Mocollo2 == 'Intravaginal o Extravaginal') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo2 == 'Intravaginal o Extravaginal');
+    } else if (req.query.Mocollo2 == 'Intravaginal o Extravaginal principalmente') {
+      pasturas = pasturas.filter(pastura => pastura.Mocollo2 == 'Intravaginal o Extravaginal principalmente');
+    }
+
+    if(req.query.ConsistenciaLigula == 'Membranacea'){
+      pasturas = pasturas.filter(pastura => pastura.ConsistenciaLigula == 'Membranacea');
+    } else if (req.query.ConsistenciaLigula == 'Disuelta en pelos') {
+      pasturas = pasturas.filter(pastura => pastura.ConsistenciaLigula == 'Disuelta en pelos');
+    } else if (req.query.ConsistenciaLigula == 'Mixta') {
+      pasturas = pasturas.filter(pastura => pastura.ConsistenciaLigula == 'Mixta');
+    }
+
+    if(req.query.FormaLigula == 'Truncada y dientada'){
+      pasturas = pasturas.filter(pastura => pastura.FormaLigula == 'Truncada y dientada');
+    } else if (req.query.FormaLigula == 'Truncada') {
+      pasturas = pasturas.filter(pastura => pastura.FormaLigula == 'Truncada');
+    } else if (req.query.FormaLigula == 'Aguda') {
+      pasturas = pasturas.filter(pastura => pastura.FormaLigula == 'Aguda');
+    } else if (req.query.FormaLigula == 'Redondeada') {
+      pasturas = pasturas.filter(pastura => pastura.FormaLigula == 'Redondeada');
+    }
+
+    if(req.query.FormaLamina == 'Linear'){
+      pasturas = pasturas.filter(pastura => pastura.FormaLamina == 'Linear');
+    } else if (req.query.FormaLamina == 'Navicular') {
+      pasturas = pasturas.filter(pastura => pastura.FormaLamina == 'Navicular');
+    } else if (req.query.FormaLamina == 'Linear-Lanceolada') {
+      pasturas = pasturas.filter(pastura => pastura.FormaLamina == 'Linear-Lanceolada');
+    } else if (req.query.FormaLamina == 'Lanceolada') {
+      pasturas = pasturas.filter(pastura => pastura.FormaLamina == 'Lanceolada');
+    }
+
+    if(req.query.TipoLamina == 'Plana'){
+      pasturas = pasturas.filter(pastura => pastura.TipoLamina == 'Plana');
+    } else if (req.query.TipoLamina == 'Navicular-Plegada') {
+      pasturas = pasturas.filter(pastura => pastura.TipoLamina == 'Navicular-Plegada');
+    } else if (req.query.TipoLamina == 'Navicular') {
+      pasturas = pasturas.filter(pastura => pastura.TipoLamina == 'Navicular');
+    } else if (req.query.TipoLamina == 'Plegada') {
+      pasturas = pasturas.filter(pastura => pastura.TipoLamina == 'Plegada');
+    } else if (req.query.TipoLamina == 'Navicular a plana') {
+      pasturas = pasturas.filter(pastura => pastura.TipoLamina == 'Navicular a plana');
+    }
+
+    if(req.query.Apice == 'Agudo'){
+      pasturas = pasturas.filter(pastura => pastura.Apice == 'Agudo');
+    } else if (req.query.Apice == 'Obtuso') {
+      pasturas = pasturas.filter(pastura => pastura.Apice == 'Obtuso');
+    }
+    
+    if(req.query.Pelos == 'Glabra'){
+      pasturas = pasturas.filter(pastura => pastura.Pelos == 'Glabra');
+    } else if (req.query.Pelos == 'Pubecente') {
+      pasturas = pasturas.filter(pastura => pastura.Pelos == 'Pubecente');
+    } else if (req.query.Pelos == 'Glabra o Pubecente') {
+      pasturas = pasturas.filter(pastura => pastura.Pelos == 'Glabra o Pubecente');
+    } else if (req.query.Pelos == 'Poco a nada') {
+      pasturas = pasturas.filter(pastura => pastura.Pelos == 'Poco a nada');
+    }
+
+    if(req.query.CicloVida == 'Anual'){
+      pasturas = pasturas.filter(pastura => pastura.CicloVida == 'Anual');
+    } else if (req.query.CicloVida == 'Perenne') {
+      pasturas = pasturas.filter(pastura => pastura.CicloVida == 'Perenne');
+    }
+
+    if(req.query.CicloProductivo == 'Invernal'){
+      pasturas = pasturas.filter(pastura => pastura.CicloProductivo == 'Invernal');
+    } else if (req.query.CicloProductivo == 'Estival') {
+      pasturas = pasturas.filter(pastura => pastura.CicloProductivo == 'Estival');
+    }
+
     res.setHeader("X-Total-Count", pasturas.length);
     res.setHeader("Content-Range", "pasturas 0-20/" + pasturas.length);
     res.send(pasturas);
